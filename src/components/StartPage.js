447c17d959e2392ed1b4953f70e88864866e27e9
Mandy Schippers <mandyschippers@192-168-1-139.tpgi.com.au>
2024-03-30 17:30:25 +1100

    retrieve data from backend apiA

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,7 +1,23 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function StartPage() {
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/hello', {
+            headers: {
+                Authorization: `Bearer not_so_secret_key` // Retrieve and use token
+            }
+        })
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching protected data', error);
+            });
+
+    })
+
     return (
         <div>
             <p>Welcome to Wingspan</p>
