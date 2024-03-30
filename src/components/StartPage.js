7657797c46ff1dddda4d9d5754f5e040b0b37e32
Mandy Schippers <mandyschippers@192-168-1-139.tpgi.com.au>
2024-03-30 14:57:24 +1100

    frontend first commit

---
+++ src/components/StartPage.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+function StartPage() {
+    return (
+        <div>
+            <p>Welcome to Wingspan</p>
+        </div>
+    )
+}
+
+export default StartPage;
